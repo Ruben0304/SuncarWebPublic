8d8e2a621aa98ff26fe0d90e5d2c3b24f31839fa
Ruben Hernandez <hernandzruben9@gmail.com>
2025-08-19 18:38:25 -0400

    Actualizar sitemap con estructura profesional: Agregar sobre-nosotros y webinfo, namespaces XML completos

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -4,43 +4,38 @@ export async function GET(request: NextRequest) {
   const baseUrl = 'https://suncarsrl.com'
   
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>${baseUrl}</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/servicios</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/projectos</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/testimonios</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/cotizacion</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.9</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/contacto</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.6</priority>
-  </url>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<!--  created with Free Online Sitemap Generator www.xml-sitemaps.com  -->
+<url>
+<loc>${baseUrl}/</loc>
+<lastmod>${new Date().toISOString()}</lastmod>
+<priority>1.00</priority>
+</url>
+<url>
+<loc>${baseUrl}/sobre-nosotros</loc>
+<lastmod>${new Date().toISOString()}</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>${baseUrl}/servicios</loc>
+<lastmod>${new Date().toISOString()}</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>${baseUrl}/contacto</loc>
+<lastmod>${new Date().toISOString()}</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>${baseUrl}/cotizacion</loc>
+<lastmod>${new Date().toISOString()}</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>${baseUrl}/webinfo</loc>
+<lastmod>${new Date().toISOString()}</lastmod>
+<priority>0.80</priority>
+</url>
 </urlset>`
 
   return new NextResponse(sitemap, {
